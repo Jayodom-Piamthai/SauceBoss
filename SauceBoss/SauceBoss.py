1ac7ee5b31f22fc6277d21a605622a44cd6a2792
CEEXAGON <acejayodom@gmail.com>
2022-01-02 22:21:52 +0700

    instruction update

--- SauceBoss/SauceBoss.py
+++ SauceBoss/SauceBoss.py
@@ -7,11 +7,13 @@ chromePath2 = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --
 
 print("//INITIALIZING SauceBoss//")
 print("MADE BY:Ace J.P.,no copyright whatsoever")
-print("for sauce opening in incognito please input the code below")
-print("for search please input: search [keyword]")
-print("for the nhentai homepage please input :home")
-print("for sauce history please input code :0000")
-print("for COMPLETE HISTORY WIPE please input :DELETE")
+print("-----Nhentai-----")
+print("open a sauce input:[sauce] ")
+print("open a home page input:home")
+print("search tab input:search [search keyword]")
+print("--SauceArchive--")
+print("open sauce history input:0000")
+print("delete history input:DELETE")
 i=0
 while i == 0 : #Rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind,rewind
     def sauceBoss(): # This is where the fun begins....
